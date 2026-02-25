71074e27ba4cac415f918a8572e8fcc8081a4561
abdullahaishan <eng.abdullah.z.aishan@gmail.com>
2026-02-25 23:54:35 +0300

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -207,7 +207,7 @@ const ContactPage = ({ developer: propDeveloper }) => {
 
           {/* قسم التعليقات */}
           <div className="bg-white/5 backdrop-blur-xl rounded-3xl p-3 py-3 md:p-10 md:py-8 shadow-2xl">
-            <Komentar />
+            <Komentar developerId={developer?.id} />
           </div>
         </div>
       </div>
