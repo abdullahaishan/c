f09eeea5381838619a38ac5291bd5c7542ed52bd
abdullahaishan <eng.abdullah.z.aishan@gmail.com>
2026-02-26 16:48:43 +0300

    Update LoadingTracker.js

--- src/components/LoadingTracker.js
+++ src/components/LoadingTracker.js
@@ -1,5 +1,5 @@
-import { supabase } from './supabase';
 
+import { supabase } from '../lib/supabase';
 export const loadingTracker = {
   // تتبع تحميل جدول معين
   async trackTable(tableName, developerId) {
