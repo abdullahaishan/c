e011e574d89a81982081d7bd3d7fbdecb998a477
abdullahaishan <eng.abdullah.z.aishan@gmail.com>
2026-02-26 19:49:28 +0300

    Update PublicPortfolio.jsx

--- src/pages/PublicPortfolio.jsx
+++ src/pages/PublicPortfolio.jsx
@@ -1,3 +1,4 @@
+import ExperienceSection from '../components/ExperienceSection'
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { developerService } from '../lib/supabase';
