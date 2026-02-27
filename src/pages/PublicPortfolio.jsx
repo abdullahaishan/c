268e2c9b92c471e45b20ba14aa32838170ac4e5f
abdullahaishan <eng.abdullah.z.aishan@gmail.com>
2026-02-27 23:27:12 +0300

    Update PublicPortfolio.jsx

--- src/pages/PublicPortfolio.jsx
+++ src/pages/PublicPortfolio.jsx
@@ -62,7 +62,7 @@ const PublicPortfolio = () => {
 
       <main className="relative z-10">
         <Home developer={developer} />
-        <About developer={developer}/>
+        {/*   <About developer={developer}/>*/}
         <Skills developer={developer} />
         <Portfolio developer={developer} />
         {/* <ExperiencePage developer={developer} />*/}
