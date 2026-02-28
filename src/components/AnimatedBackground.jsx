add2102f1b12f1aa8645ae6ee793d10d344f80d4
abdullahaishan <eng.abdullah.z.aishan@gmail.com>
2026-03-01 02:39:12 +0300

    Update AnimatedBackground.jsx

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -69,9 +69,7 @@ const AnimatedBackground = () => {
 					className="absolute -bottom-10 right-20 w-96 h-96 bg-indigo-600 rounded-full mix-blend-multiply filter blur-[128px] opacity-20 md:opacity-10 hidden sm:block animate-pulse-slow animation-delay-6000"
 				></div>
 			</div>
-			
-			{/* شبكة الخلفية */}
-			<div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f10_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f10_1px,transparent_1px)] bg-[size:24px_24px]"></div>
+		
 		</div>
 	)
 }
