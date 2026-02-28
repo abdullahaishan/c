43ec70744510f1c9bdb7fbc3b5b7688692170838
abdullahaishan <eng.abdullah.z.aishan@gmail.com>
2026-02-28 16:40:16 +0300

    Update ProjectDetail.jsx

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { projectService } from "../../lib/supabase";
+import { projectService } from "../lib/supabase";
 import {
   ArrowLeft,
   ExternalLink,
