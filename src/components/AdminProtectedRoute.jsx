3461f57d5765b254b7c3487ba3e45413ade962e4
abdullahaishan <eng.abdullah.z.aishan@gmail.com>
2026-03-03 00:50:50 +0300

    Update AdminProtectedRoute.jsx

--- src/components/AdminProtectedRoute.jsx
+++ src/components/AdminProtectedRoute.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth'
 import { useAdminAuth } from '../hooks/useAdminAuth'
 import LoadingScreen from './LoadingScreen'
 
-const AdminProtectedRoute = ({ children }) => {
-  const { admin, loading } = useAdminAuth()
+const ProtectedRoute = ({ children, adminOnly = false }) => {
+  const { user, loading } = useAuth()
+  const { admin, loading: adminLoading } = useAdminAuth()
   const location = useLocation()
 
-  if (loading) {
+  if (loading || (adminOnly && adminLoading)) {
     return <LoadingScreen />
   }
 
-  if (!admin) {
-    return <Navigate to="/admin/login" state={{ from: location }} replace />
+  if (adminOnly) {
+    if (!admin) {
+      return <Navigate to="/admin/login" state={{ from: location }} replace />
+    }
+    return children
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />
   }
 
   return children
 }
 
-export default AdminProtectedRoute
+export default ProtectedRoute
