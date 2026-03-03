e3d4c23094379463fcbe6c9853869d30a9c0202d
abdullahaishan <eng.abdullah.z.aishan@gmail.com>
2026-03-03 04:53:28 +0300

    Update Plans.jsx

--- src/pages/admin/Plans.jsx
+++ src/pages/admin/Plans.jsx
@@ -770,25 +770,6 @@ const Plans = () => {
   )
 }
 
-// تعريف الأيقونات المفقودة
-const Briefcase = (props) => (
-  <svg {...props} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-    <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
-    <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
-  </svg>
-)
 
-const GraduationCap = (props) => (
-  <svg {...props} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-    <path d="M22 10v6M2 10l10-5 10 5-10 5-10-5z"></path>
-    <path d="M6 12v5c3 3 9 3 12 0v-5"></path>
-  </svg>
-)
-
-const Star = (props) => (
-  <svg {...props} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-  </svg>
-)
 
 export default Plans
