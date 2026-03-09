8e717d080374b6f79a7d6f716e7ab5983efda977
abdullahaishan <eng.abdullah.z.aishan@gmail.com>
2026-03-09 22:04:00 +0300

    Update Projects.jsx

--- src/pages/dashboard/Projects.jsx
+++ src/pages/dashboard/Projects.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useAuth } from '../../hooks/useAuth'
 import { projectService, storageService } from '../../lib/supabase'
 import Swal from 'sweetalert2'
 import {
